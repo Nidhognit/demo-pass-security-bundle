a45a03411f5cfab07f49fd4560131a9eb03618b7
nidhognit <failthen@gmail.com>
2017-03-01 15:39:26 +0200

    remove unused bundels

--- src/DemoBundle/Resources/js/demoPassword.js
+++ src/DemoBundle/Resources/js/demoPassword.js
@@ -7,15 +7,21 @@ $(document).ready(function () {
             url: "/check_password",
             statusCode: {
                 500: function () {
-                    //todo
+                    alert('Something went wrong, this is 500 error')
                 },
                 403: function () {
-                    //todo
+                    alert('Something went wrong, this is 403 error')
                 }
             },
             success: function (data) {
                 var number = data.number;
-                alert(number);
+                var $answerPlace = $('.server-answer');
+                $answerPlace.empty();
+                if (number > 0) {
+                    $('<span>').addClass('text-danger').text('This password was found in list, his number ' + number).appendTo($answerPlace);
+                } else {
+                    $('<span>').addClass('text-success').text('This is a good password, it was not found in the list').appendTo($answerPlace);
+                }
             }
         });
     })
